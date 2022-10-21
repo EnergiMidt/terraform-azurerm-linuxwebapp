ea4bc03b8cb2d169e45dfbf441da9dbf631c758d
Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@computas.com>
2022-10-21 12:40:23 +0200

    feature: terraform graph

--- generate-graph.sh
+++ generate-graph.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
 terraform graph -draw-cycles -type=plan \
-    | dot -Tsvg -Ecolor=red -Earrowhead=diamond -Kdot >terraform-graph.svg
+    | dot -Tsvg -Ecolor=red -Earrowhead=diamond -Kdot \
+        >"graph-${PWD##*/}.svg"
 
 terraform graph -draw-cycles -type=plan \
-    | dot -Tpng -Ecolor=red -Earrowhead=diamond -Kdot >terraform-graph.png
+    | dot -Tpng -Ecolor=red -Earrowhead=diamond -Kdot \
+        >"graph-${PWD##*/}.png"
 
 terraform graph -draw-cycles -type=plan \
     | terraform-graph-beautifier \
         --exclude="module.root.provider" \
         --output-type=cyto-html \
-        >terraform-graph.html
+        >"graph-${PWD##*/}.html"
