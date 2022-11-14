a7546070a535e042ffb55e9b15caf7253a2deaba
Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@computas.com>
2022-11-14 11:15:44 +0100

    fix: identity dynamic block

--- main.tf
+++ main.tf
@@ -14,8 +14,8 @@ resource "azurerm_linux_web_app" "linux_web_app" {
     for_each = try(var.identity, null) == null ? [] : [1]
 
     content {
-      type         = identity.value.type
-      identity_ids = try(identity.value.managed_identities, null)
+      type         = var.identity.value.type
+      identity_ids = try(var.identity.value.managed_identities, null)
     }
   }
 
