be4cc5a1ae12441184ae1ddd62037d4a240432f6
Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@computas.com>
2023-01-11 13:39:40 +0100

    chore: optional site config argument

--- main.tf
+++ main.tf
@@ -19,9 +19,8 @@ resource "azurerm_linux_web_app" "linux_web_app" {
   }
 
   site_config {
-    always_on                                     = lookup(try(var.configuration.site_config, {}), "always_on", true)
-    container_registry_managed_identity_client_id = lookup(try(var.configuration.site_config, {}), "container_registry_managed_identity_client_id", null)
-    container_registry_use_managed_identity       = lookup(try(var.configuration.site_config, {}), "container_registry_use_managed_identity", false)
+    always_on                               = lookup(try(var.configuration.site_config, {}), "always_on", true)
+    container_registry_use_managed_identity = lookup(try(var.configuration.site_config, {}), "container_registry_use_managed_identity", false)
   }
 
   tags = var.tags
