0e13d826fbe27105247f1d32808fac781e2b3cb4
Peder Lång Skeidsvoll <pls@computas.com>
2023-01-13 13:22:14 +0100

    Added cert binding

--- main.tf
+++ main.tf
@@ -34,3 +34,13 @@ resource "azurerm_app_service_custom_hostname_binding" "custom_hostname" {
   app_service_name    = azurerm_linux_web_app.linux_web_app.name
   resource_group_name = var.resource_group.name
 }
+
+resource "azurerm_app_service_managed_certificate" "cert" {
+  custom_hostname_binding_id = azurerm_app_service_custom_hostname_binding.custom_hostname.id
+}
+
+resource "azurerm_app_service_certificate_binding" "cert_binding" {
+  hostname_binding_id = azurerm_app_service_custom_hostname_binding.custom_hostname.id
+  certificate_id      = azurerm_app_service_managed_certificate.cert.id
+  ssl_state           = "SniEnabled"
+}
