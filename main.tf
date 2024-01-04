ae849559263b44afe8213a12542e3fe4bd3e3b1f
Tore Eide Andersen <tore.eide.andersen@kantega.no>
2024-01-04 10:53:55 +0100

    Dynamic logs block

--- main.tf
+++ main.tf
@@ -38,11 +38,14 @@ resource "azurerm_linux_web_app" "linux_web_app" {
         for_each = try(var.configuration.logs.http_logs, null) != null ? [var.configuration.logs.http_logs] : []
         content {
           dynamic "file_system" {
-            for_each = try(var.configuration.logs.http_logs.file_system, null) != null ? [var.configuration.logs.http_logs.file_system] : []
+            for_each = try(var.configuration.logs.http_logs.file_system, null) != null ? [
+              var.configuration.logs.http_logs.file_system
+            ] : []
             content {
               retention_in_days = var.configuration.logs.http_logs.file_system.retention_in_days
-              retention_in_mb = var.configuration.logs.http_logs.file_system.retention_in_mb
+              retention_in_mb   = var.configuration.logs.http_logs.file_system.retention_in_mb
             }
+          }
         }
       }
     }
